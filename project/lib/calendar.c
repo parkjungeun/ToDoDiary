ed4409412c25ba150581d5d0b549d5ab055d4289
Jungeun Park <jungeun3814@naver.com>
2017-11-13 01:24:26 -0800

    second change calendar.c

--- project/lib/calendar.c
+++ project/lib/calendar.c
@@ -15,21 +15,41 @@ void calandar(int year,int month)
 	if(i==1)
 	{
 	    if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) //윤년
-	    {
 		days[1] = 29;
-	    }
 	    else
-	    {
 		days[1] = 28;
-	    }
 	}
 	days += day[i];
     }
+
+
+    /* 제목 출력 */
+    printf("\n\t  %d년  %d월\n", year, month);
+    printf("\t==============\n");
+    printf("-----------------------------\n");
+    printf(" SUN MON TUE WED THU FRI SAT \n");
+    printf("-----------------------------\n");
+
 
     // 7일씩 나눠주면 전달에서 다음달로 넘어갈 때 첫주 칸을 채울 cnt
     first_date = days % 7;
 
     // 1년 1월 1일은 월요일 (시작은 일요일부터)
+    for(int i = 0; i <= first_data; i++)
+    {
+	printf("\t");
+	cnt++;
+    } 
 
+    for(int i = 0; i <= day[month - 1]; i++)
+    {
+	if(cnt % 7 == 0)
+	    printf("\n");
 
+	printf("%4d",i);
+	cnt++;
+    }
+
+    printf("\n\n");
+    return;
 }
