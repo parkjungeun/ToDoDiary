75aff82a379b60eb949d2ceee0086cc03ec0c3d8
hy <z4ckdnjsz@naver.com>
2017-11-13 21:59:45 -0800

    login4

--- project/src/login.cpp
+++ project/src/login.cpp
@@ -11,11 +11,19 @@ int main(int argc,char *argv[]){
       cout<<"parameter : ID PW"<<endl;
       exit(0);
    }
+
+   //test id and pw
    string id="hy";
    string pw="1234";
+
+   int idfd=open("~/userID",O_RDONLY);//path???????????
+   read(idfd,id,);//반복하면서 읽기
+   int pwfd=open();
+   read(pwfd,pw,_;//반복하면서 읽기
+
    if(argv[1]==id){
       if(argv[2]==pw){
-         while{
+         while(){
             //실행파일들이 모아져있는 디렉토리의 파일이름을 다 읽어올 동안 반복
             chmod();
          }
@@ -24,7 +32,7 @@ int main(int argc,char *argv[]){
          cout<<"wrong password"<<endl;
    }
   else
-   cout<<"wrong password"<<endl;
+   cout<<"wrong ID"<<endl;
 
 
    return 0;
