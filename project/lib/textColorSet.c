ca8739bb88cb01f844caa0008969a8e69e6be6ae
hy <z4ckdnjsz@naver.com>
2017-11-14 22:32:09 -0800

    add textcolor setting function

---
+++ project/lib/textColorSet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+// colorNum 1 Red
+// colorNum 0 Blue
+void textColorSet(char *c,int colorNum) {
+
+        if(colorNum==0){
+        	printf("\033[%dm%s\033[0m",94, c);
+	}
+	else if(colorNum==1){
+		printf("\033[%dm%s\033[0m",91,c);
+	}
+	return;
+}
+
