f7d6a774e4930aad3eef909dbea0048951b5048f
Jungeun Park <jungeun3814@naver.com>
2017-11-15 17:27:17 -0800

    main

--- project/src/main.c
+++ project/src/main.c
@@ -3,6 +3,10 @@
 int main()
 {
     int request=0, res=0;
+    int year, month;
+    char day[50], plan[50];
+    char re_day[50], re_plan[50];
+    int Men_month, Men_day;
 
 //    res = login();
 
@@ -23,20 +27,28 @@ int main()
 
 	    switch(request)
 	    {
-		case 1:
-		    calendar();		break;
+		case 1:	
+		    printf("년 월을 입력하시오 : ");
+		    scanf("%d %d",&year,&month);
+		    calendar(year,month);		break;
 		case 2:
 		    //output();		break;
 		case 3:
+		    printf("날짜와 일정을 입력하시오 : ");
+		    scanf("%s %s",&day, &plan);
 		    //input();		break;
 		case 4:
+		    printf("반복되는 날짜와 일정을 입력하시오 : ");
+		    scanf("%s %s",&re_day, &re_plan);
 		    //recurringEvent();	break;
 		case 5:
+		    printf("월경일을 입력하시오 (월 일) : ");
+		    scanf("%d %d",&Men_month, &Men_day);
 		    //Menstruation();	break;
 		case 6:
-		    //return 0;
+		    return 0;gcc -
 		default:
-		    //break;
+		    break;
 	    }
 	}	
     }
