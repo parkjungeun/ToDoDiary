d51f0fccea3866101ca596c927a22cedd7e6994a
hy <z4ckdnjsz@naver.com>
2017-11-20 03:14:47 -0800

    login func

---
+++ project/include/login.h
@@ -0,0 +1,6 @@
+#ifndef LOGIN_H
+#define LOGIN_H
+
+bool login(int password);
+
+#endif
---
+++ project/lib/login.c
@@ -0,0 +1,6 @@
+#include "login.h"
+
+bool login(int password){
+	if(password==1234) return true;
+	else return false;
+}
